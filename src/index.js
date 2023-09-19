4d01d4ad4d400e147593b649a6cef37e74664724
sarahBOKAR-THIRE <sbokarthire@gmail.com>
2023-09-19 15:02:51 +0200

    update install

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
+import '../src/scss/styles.scss'//Import scss
+import * as bootstrap from 'bootstrap' //bootstrap JS
 
 function component() {
     const element = document.createElement('div');
+    element.classList.add('container');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
